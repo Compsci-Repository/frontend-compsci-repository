7d560c76a4353de5c77a66d2626377267d4de505
eduardor0cha <c.eduardo_rn@outlook.com>
2025-05-27 01:34:15 -0300

    fix/reset `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { Input } from "./view/components";
-import ToggleThemeButton from "./view/components/ToggleThemeButton/ToggleThemeButton";
-
 function App() {
   return (
     <>
-      <h1>Compsci Repository :D</h1>
-      <Input placeholder="Procurar material" />
-      <ToggleThemeButton />
+      <h1>
+        Compsci Repository :D
+      </h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
+
